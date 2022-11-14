1038bdc795e31d5880c6093de388b5ab82959edc
arsen-melk <arsenmelk001@gmail.com>
2022-11-14 19:45:35 +0400

    3 հատ առաջադրանք

---
+++ script.js
@@ -0,0 +1,40 @@
+ 
+ /*
+ //Առաջին առաջադրանք
+
+for(i=0;i<=100;i+=2) {
+    console.log(i);
+}
+
+
+
+ //Երկրորդ առաջադրանք(բացառությամբ վերջին պահանջի)
+
+let text1;
+
+while(true){
+    text1=Number(prompt("Գրեք ցանկացած թիվ"));
+    
+     if(text1===0){
+        break;
+    }    
+} 
+
+
+    
+
+      //Երրորդ առաջադրանք
+
+
+    userName= Number(prompt("Մուտքագրեք թիվ 0-59 միջակայքից"));
+
+    if(userName>=0 & userName<=14){
+        console.log("Թիվը գտնվում է առաջին քառորդում");
+    } else if(userName>=15 & userName<=29){
+        console.log("Թիվը գտնվում է երկրորդ քառորդում");
+    }else if(userName>=30 & userName<=44){
+        console.log("Թիվը գտնվում է երրրորդ քառորդում");
+    }else if(userName>=45 & userName<=59){
+        console.log("Թիվը գտնվում է չորորդ քառորդում");
+    }else{console.log("Թիվը չի գտնվում սահմանված միջակայքում");}
+      */
